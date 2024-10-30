e2ac3c9f7f3923c140dd57c8c5a83a1ea6150b68
dougalcaleb <dougalcaleb@gmail.com>
2024-10-30 11:43:06 -0600

    Version 1.1.0 * Added functionality to run on the same machine as the Minecraft server * Added displaying server IP address * Updated to Discord.js 14 * Validated on PaperMC 1.21.1

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Spigot Status Bot
 A Discord bot to help you keep tabs on your Spigot-based Minecraft server.
 
-Tested on Spigot and PaperMC 1.17.1
+Tested on PaperMC 1.21.1
 
 Features:
 - Shows online status
@@ -9,16 +9,20 @@ Features:
 - Displays ticks per second and memory usage
 - Displays online players
 - Clean embed message format
+- Can be used either on the same machine as the Minecraft server, or another machine
 
 # Setup
 Because I do not own the hardware to host this bot on multiple servers at a time, you will need to register your own Discord bot. However, this is very simple to do:
 - Go to the [Discord Developer Portal](https://discord.com/developers/docs/intro).
 - On the left side, go to Applications and click New Application at the top right. Walk through the steps to create your bot.
+	- Under OAuth2, select the `bot` scope and enable the permissions `View Channels`, `Send Messages`, and `Manage Messages`.
+	- Under Bot, enable `Message Content Intent`
 - Once your bot is created, copy and save your bot's Token (but be careful to keep it secret and secure).
+- Invite your bot to your Discord server.
 
-Verify that your bot can be added to your Discord server. To get the bot up and running:
+To get the bot up and running:
 - Extract the code from ```src/bot``` to the hardware you want to run it from.
-- Place your token in the appropriate place in the config file.
+- Place your token in the appropriate place in the token config file.
 - Configure your settings in the config file and in your Discord server. (See [settings](#settings))
 - Use ```node bot.js``` in the terminal to run the bot. 
 
@@ -39,16 +43,14 @@ You are free to expand upon and modify the software provided, as detailed in the
 
 Setting | Type | Description
 --------|------|------------
-serverName | string | The name of the server that the Discord bot will display as its status
-thumbnail | string | URL of an image to use for the embed's thumbnail
-serverAddress | string | Web address of the Minecraft server
+serverName | string | The name of the server that the Discord bot will display as its Rich Presence status
+serverAddress | string | Web address of the Minecraft server. Enter this ONLY if the Discord bot is running on a different machine than the Minecraft server is running.
 serverPort | number | Port that the Minecraft server is running on
-verifyStatus | boolean | Toggles whether the bot will ping the Spigot server to verify that it is online or offline<br/> <details><summary>Expand description</summary><br/>This option helps to verify that the server continues to run. In the event that the server encounters a catastrophic error (such as network failure or the computer shutting off), the update that the server has shut down will not be sent successfully. Regular verifications will ensure that an incorrect status is not being displayed. <br/><br/>If your server's port cannot be pinged, for security reasons or otherwise, disable this option.</details>
+verifyStatus | boolean | Toggles whether the bot will ping the Spigot server to verify that it is online or offline<br/> <details><summary>Expand description</summary><br/>This option helps to verify that the server continues to run. In the event that the server encounters a catastrophic error (such as network failure or the computer shutting off), the update that the server has shut down will not be sent successfully. Regular verifications will ensure that an incorrect status is not being displayed. If the bot and server are on the same machine, this may be less useful.<br/><br/>If your server's port cannot be pinged, for security reasons or otherwise, disable this option.</details>
 verifyInterval | number | The amount of time (in seconds) between verification pings to the server
 verificationTimeout | number | The timeout (in seconds) that the bot will wait until a ping is considered unsuccessful
 botChannel | string | The ID of the Discord channel that the bot should post its message to <br/> <details><summary>Expand description</summary><br/>Ensure that only the bot can send messages to this channnel. Other messages will break the bot.<br/><br/>To get the ID, enable Discord's Developer Mode, then right click channel > "Copy ID".</details>
 webhookChannel | string | The ID of the channel that the Minecraft server will send updates to
-botToken | string | Your Discord bot's login token
 
 <br/>
 <br/>
--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,240 +1,268 @@
 const Net = require("net");
-const Discord = require("discord.js");
-const Client = new Discord.Client();
+const { Client, GatewayIntentBits, Events } = require('discord.js');
 const Filesystem = require("fs");
 const config = JSON.parse(Filesystem.readFileSync("./config.json", "utf-8"));
+const token = JSON.parse(Filesystem.readFileSync("./token.json", "utf-8"))
+const os = require('os');
 
-let outputs = {status: null, errors: null};
+
+let outputs = { status: null, errors: null };
+// state
 let server = {
 	lastOnline: 0,
 	online: false,
 	players: [],
 	health: [],
 	message: [],
-   msgChannel: null,
-   errors: [],
-   verifiedAt: Date.now(),
+	msgChannel: null,
+	errors: [],
+	verifiedAt: new Date().toISOString(),
 };
+// internal config
 const intConfig = {
-   onlineColor: "0x00b7ff",
-   offlineColor: "0xff0000",
-   errorColor: "0x800808",
+	onlineColor: 0x00b7ff,
+	offlineColor: 0xff0000,
+	errorColor: 0x800808,
 };
 
-Client.once("ready", () => {
-   console.log("Bot active");
+// Create a new client
+const BotClient = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+
+// Bot ready
+BotClient.once(Events.ClientReady, (readyClient) => {
+	console.log("Bot active");
 
 	// Get correct channel
-	server.msgChannel = Client.channels.cache.get(config.botChannel);
+	server.msgChannel = BotClient.channels.cache.get(config.botChannel);
 
-   // Hook existing message
-   Util.fetchBotMessage((msg) => {
-      if (msg) {
-         outputs.status = msg;
-         Util.message();
-      }
-   });
+	// Hook existing message
+	Utility.fetchBotMessage((msg) => {
+		if (msg) {
+			outputs.status = msg;
+			Utility.message();
+		}
+	});
 
 	// Set status
-   Client.user.setActivity(config.serverName, { type: "WATCHING" });
-   
-   // Verification ping
-   if (config.verifyStatus) {
-      setInterval(() => {
-         Util.pingServer([config.serverPort, config.serverAddress], (result) => {
-            server.online = result;
-            server.verifiedAt = Date.now();
-            console.log(`Server is online: ${result}`);
-   
-            Util.message();
-         });
-      }, config.verifyInterval * 1000);
-   }
+	BotClient.user.setActivity(config.serverName, {type: "WATCHING"});
+
+	// Verification ping
+	if (config.verifyStatus) {
+		setInterval(() => {
+			const pingAddress = config.serverAddress || Utility.getIPAddress();
+			Utility.pingServer([config.serverPort, pingAddress], (result) => {
+				server.online = result;
+				server.verifiedAt = new Date().toISOString();
+				console.log(`Server is online: ${result}`);
+
+				Utility.message();
+			});
+		}, config.verifyInterval * 1000);
+	}
 });
 
 // React to input
-Client.on("message", (message) => {
+BotClient.on(Events.MessageCreate, (message) => {
+	// Only get messages from the correct channel
 	if (message.channel.id != config.webhookChannel) return;
 
-   let command = message.content.split("|");
-   
-   server.verifiedAt = Date.now();
+	let command = message.content.split("|");
 
-   let player = "";
+	server.verifiedAt = new Date().toISOString();
 
-   console.log("Health update recieved: " + command.join(" | "));
+	let player = "";
 
-   // Actions for different commands
-   switch (command[0]) {
-      case "SERVER_INIT":
-         server.lastOnline = Date.now();
-         server.online = true;
-         server.players = [];
-			break;
-      case "SERVER_SHUTDOWN":
-         server.lastOnline = Date.now();
-         server.online = false;
-         server.players = [];
-			break;
-      case "PLAYER_JOIN":
-         server.lastOnline = Date.now();
-         server.online = true;
-         player = Util.parsePlayerName(command[1]);
-         server.players.push(player);
-			break;
-      case "PLAYER_LEAVE":
-         server.lastOnline = Date.now();
-         server.online = true;
-         player = Util.parsePlayerName(command[1]);
-         server.players = server.players.filter((playerOnline) => {
-            return playerOnline != player;
-         });
-			break;
-      case "SERVER_REPORT":
-         server.lastOnline = Date.now();
-         server.online = true;
-         server.health[0] = command[1];
-         server.health[1] = command[2];
-         break;
-      case "SET_MESSAGE":
-         server.message[parseInt(command[1])] = command[2];
-         break;
-      case "CLEAR_ERRORS":
-         server.errors = [];
-         Util.deleteErrorMsg();
-         break;
-      default:
-         console.log("Invalid operator: " + command[0]);
-   }
+	console.log("Health update received: " + command.join(" | "));
 
-   Util.message();
+	// Actions for different commands
+	switch (command[0]) {
+		case "SERVER_INIT":
+			server.lastOnline = new Date().toISOString();
+			server.online = true;
+			server.players = [];
+			break;
+		case "SERVER_SHUTDOWN":
+			server.lastOnline = new Date().toISOString();
+			server.online = false;
+			server.players = [];
+			break;
+		case "PLAYER_JOIN":
+			server.lastOnline = new Date().toISOString();
+			server.online = true;
+			player = Utility.parsePlayerName(command[1]);
+			server.players.push(player);
+			break;
+		case "PLAYER_LEAVE":
+			server.lastOnline = new Date().toISOString();
+			server.online = true;
+			player = Utility.parsePlayerName(command[1]);
+			server.players = server.players.filter((playerOnline) => {
+				return playerOnline != player;
+			});
+			break;
+		case "SERVER_REPORT":
+			server.lastOnline = new Date().toISOString();
+			server.online = true;
+			server.health[0] = command[1];
+			server.health[1] = command[2];
+			break;
+		case "SET_MESSAGE":
+			server.message[parseInt(command[1])] = command[2];
+			break;
+		case "CLEAR_ERRORS":
+			server.errors = [];
+			Utility.deleteErrorMsg();
+			break;
+		default:
+			console.log("Invalid operator: " + command[0]);
+	}
+
+	Utility.message();
 });
-
-
 
 // Common functions
 class Utility {
-   constructor() { }
-   
-   // Send/ edit a status message
-   message() {
-      // Server data
-      let lines = [
-         {
-            name: "Connection Status: \u200b\u200b\u200b",
-            value: server.online ? ":white_check_mark: Online" : ":x: Offline",
-            inline: true,
-         }
-      ];
+	// Send/ edit a status message
+	static message() {
+		// Server data
+		let lines = [
+			{
+				name: "Connection Status: \u200b\u200b\u200b",
+				value: server.online ? ":white_check_mark: Online" : ":x: Offline",
+				inline: true,
+			},
+		];
 
-      // Online data
-      if (server.online) {
-         lines.push({
-            name: "Current TPS:",
-            value: ":signal_strength: " + (server.health[0] || "Waiting...")
-         });
-         lines.push({
-            name: "Current memory usage:",
-            value: ":chart_with_downwards_trend: " + (server.health[1] || "Waiting...")
-         });
-         lines.push({
-            name: "Players online:",
-            value: server.players.length > 0 ? server.players.join("\n") : "\u200b"
-         });
-      } else {
-         lines.push({
-            name: "Last online:",
-            value: Util.getTimeSince(server.lastOnline)
-         });
-      }
+		// Online data
+		if (server.online) {
+			lines.push({
+				name: "Current TPS:",
+				value: ":signal_strength: " + (server.health[0] || "Waiting..."),
+			});
+			lines.push({
+				name: "Current memory usage:",
+				value: ":chart_with_downwards_trend: " + (server.health[1] || "Waiting..."),
+			});
+			lines.push({
+				name: "Players online:",
+				value: server.players.length > 0 ? server.players.join("\n") : "\u200b",
+			});
+			lines.push({
+			    name: "IP address:",
+			    value: Utility.getIPAddress() + ":25565",
+			});
+		} else {
+			lines.push({
+				name: "Last online:",
+				value: Utility.getTimeSince(new Date(server.lastOnline).valueOf()),
+			});
+			lines.push({
+			    name: "IP address:",
+			    value: Utility.getIPAddress() + ":25565",
+			});
+		}
 
-      // Embed data
-      let embedData = {
-         color: server.online ? intConfig.onlineColor : intConfig.offlineColor,
-         title: "Server Status",
-         thumbnail: {
-            url: config.thumbnail
-         },
-         fields: lines,
-         timestamp: server.verifiedAt,
-         footer: {
-            text: "Verified: "
-         }
-      }
+		// Embed data
+		let embedData = {
+			color: server.online ? intConfig.onlineColor : intConfig.offlineColor,
+			title: "Server Status",
+			fields: lines,
+			timestamp: server.verifiedAt,
+			footer: {
+				text: "Verified: ",
+			},
+		};
 
-      // Custom message
-      if (server.message.length > 0) {
-         embedData.description = server.message.join("\n");
-      }
+		// Custom message
+		if (server.message.length > 0) {
+			embedData.description = server.message.join("\n");
+		}
 
-      // Create embed
-      let embed = new Discord.MessageEmbed(embedData);
+		// Create embed
 
-      // Send a new message or edit the existing one
-      if (outputs.status) {
-         outputs.status.edit(embed).catch((error) => {
-            console.log("Message edit failed");
-            console.log(error);
-            Util.error(error);
-         });
-      } else {
-         server.msgChannel.send(embed).then((message) => {
-            outputs.status = message;
-         }).catch((error) => {
-            console.log("Message send failed");
-            console.log(error);
-            Util.error(error);
-         });
-      }      
-   }
+		// Send a new message or edit the existing one
+		if (outputs.status) {
+			// edit existing embed
+			outputs.status.edit({ embeds: [embedData] }).catch((error) => {
+				console.log("Message edit failed");
+				console.log(error);
+				Utility.error(error);
+			});
+		} else {
+			// create new embed
+			server.msgChannel
+				.send({ embeds: [embedData] })
+				.then((message) => {
+					outputs.status = message;
+				})
+				.catch((error) => {
+					console.log("Message send failed");
+					console.log(error);
+					Utility.error(error);
+				});
+		}
+	}
 
-   // Send an error message
-   error(e) {
-      let str = `HTTP Status: ${e.httpStatus} Code: ${e.code} Path: ${e.path}`;
-      if (!server.errors.includes(str)) {
-         server.errors.push(str);
-      }
-      let embedData = {
-         color: intConfig.errorColor,
-         title: "ERRORS",
-         fields: [
-            {
-               name: "Current:",
-               value: server.errors.join("\n\n")
-            }
-         ]
-      }
-      let embed = new Discord.MessageEmbed(embedData);
-      if (outputs.errors) {
-         outputs.errors.edit(embed).catch((error) => {
-            console.log("Message edit failed");
-            console.log(error);
-            Util.error(error);
-         });
-      } else {
-         server.msgChannel.send(embed).then((message) => {
-            outputs.errors = message;
-         }).catch((error) => {
-            console.log("Message send failed");
-            console.log(error);
-            Util.error(error);
-         });
-      }      
-   }
+	// Send an error message
+	static error(e) {
+		let str = `HTTP Status: ${e.httpStatus} Code: ${e.code} Path: ${e.path}`;
+		if (!server.errors.includes(str)) {
+			server.errors.push(str);
+		}
+		let embedData = {
+			color: intConfig.errorColor,
+			title: "ERRORS",
+			fields: [
+				{
+					name: "Current:",
+					value: server.errors.join("\n\n"),
+				},
+			],
+		};
+		
+		if (outputs.errors) {
+			outputs.errors.edit({ embeds: [embedData] }).catch((error) => {
+				console.log("Error message edit failed");
+				console.log(error);
+				Utility.error(error);
+			});
+		} else {
+			server.msgChannel
+				.send({ embeds: [embedData] })
+				.then((message) => {
+					outputs.errors = message;
+				})
+				.catch((error) => {
+					console.log("Error message send failed");
+					console.log(error);
+					Utility.error(error);
+				});
+		}
+	}
 
-   deleteErrorMsg() {
-      if (!outputs.errors) return;
+	static deleteErrorMsg() {
+		if (!outputs.errors) return;
 
-      outputs.errors.delete().then(() => {
-         console.log("Error message delete successful");
-         outputs.errors = null;
-      }).catch(() => {
-         console.log("Could not delete error message");
-      });
-   }
+		outputs.errors
+			.delete()
+			.then(() => {
+				console.log("Error message delete successful");
+				outputs.errors = null;
+			})
+			.catch(() => {
+				console.log("Could not delete error message");
+			});
+	}
 
 	// Returns a string that says how long it has been since the given timestamp
-	getTimeSince(date) {
+	static getTimeSince(date) {
 		if (date == 0) {
 			return "Unknown";
 		}
@@ -267,25 +295,25 @@ class Utility {
 	}
 
 	// Returns a time + date string from a timestamp (hh:mm:ss xm, mm/dd/yyyy)
-	getTime(date) {
+	static getTime(date) {
 		if (date == 0) {
 			return "";
 		}
 		return new Date(date).toLocaleTimeString("en-US") + ", " + new Date(date).toLocaleDateString("en-US");
-   }
-   
-   // Get the last bot message
-   fetchBotMessage(callback) {
-      server.msgChannel.messages.fetch({limit: 1}).then((messages) => {
-         messages.forEach((message) => {
-            callback(message);
-         });
-      });
-   }
+	}
+
+	// Get the last bot message
+	static fetchBotMessage(callback) {
+		server.msgChannel.messages.fetch({limit: 1}).then((messages) => {
+			messages.forEach((message) => {
+				callback(message);
+			});
+		});
+	}
 
 	// Ping the server socket to verify it is active
-	pingServer(ip, callback) {
-      let socket = new Net.Socket();
+	static pingServer(ip, callback) {
+		let socket = new Net.Socket();
 		socket.setTimeout(config.verificationTimeout * 1000);
 		socket
 			.on("connect", () => {
@@ -302,7 +330,7 @@ class Utility {
 	}
 
 	// Parse a player's name to prevent text formatting
-	parsePlayerName(name) {
+	static parsePlayerName(name) {
 		let fixedName = name.trim();
 		fixedName = fixedName.split("").map((char) => {
 			if (char === "*" || char === "_") {
@@ -313,8 +341,19 @@ class Utility {
 		fixedName = fixedName.join("");
 		return fixedName;
 	}
+
+	// Get the IP address of the machine running the bot
+	static getIPAddress() {
+		const networkInterfaces = os.networkInterfaces();
+		for (const interfaceName in networkInterfaces) {
+			for (const interfaceInfo of networkInterfaces[interfaceName]) {
+				if (interfaceInfo.family === "IPv4" && !interfaceInfo.internal) {
+					return interfaceInfo.address;
+				}
+			}
+		}
+		return "IP address not found";
+	}
 }
 
-const Util = new Utility();
-
-Client.login(config.botToken);
+BotClient.login(token.botToken);
